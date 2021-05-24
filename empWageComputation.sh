1497fc03dabe48d20ba636b74783aa983c22e525
BootCampBL <minibootcamp@bridgelabz.com>
2021-05-24 18:16:32 +0530

    Added emp present or absent feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,13 @@
 
 
 echo "Welcome to shell script of empwage"
+
+isEmployeePresent=$((RANDOM%2))
+
+if [ $isEmployeePresent -eq 1 ]
+then
+    echo "Employee is Present"
+else
+    echo "Employee is Absent"
+fi
+
